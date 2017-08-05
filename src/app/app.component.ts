6239b81fcd3b113137afd810cde9d333b7075f1b
Srikanth Shankar <srikanth.shankar2@gmail.com>
2017-08-05 14:01:08 +1000

    basic commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+ import {Post} from './post';
+ import {NgForm} from '@angular/common';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl:'./app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Angular';
+  categories=['tech', 'entertainment', 'finance'];
+  model=new Post(1, 'post 1', this.categories[0], 'post body', 'srikanth');
+  submitted = false;
+}
